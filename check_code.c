30f25e4ca074d8799e6cf7921fbc2ce9a0918ccc
Sumayah07 <muhydeensumayah27@gmail.com>
2023-09-20 05:16:05 -0700

    check_code.c

--- check_code.c
+++ check_code.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Print Intgers number
+ * main - Printing the Intgers value
  *
- * return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int a = 10;
 
-	printf("Printing the Integer value %d, a);
+	printf("Printing the Integer value %d", a);
 	return (0);
 }
