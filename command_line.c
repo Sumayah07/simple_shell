525cc8ea7e9091d2f01b6380a2cd1830780f4f34
Sumayah07 <muhydeensumayah27@gmail.com>
2023-09-20 07:43:27 -0700

    command_line

--- command_line.c
+++ command_line.c
@@ -2,20 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - Interpreting the pid_t command line
+ * main - PID
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int num = 27;
-
 	pid_t my_process_id;
 
 	my_process_id = getpid();
-
-	printf("%d\n", num);
-	printf("The child process id is %u", my_process_id);
-
+	printf("%u\n", my_pid);
 	return (0);
 }
