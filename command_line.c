5874806de1a9f4fcd37c2f612779fd9725a96b10
Sumayah07 <muhydeensumayah27@gmail.com>
2023-09-23 06:17:20 -0700

    command_line.c

--- command_line.c
+++ command_line.c
@@ -1,16 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-/**
- * main - PID
- *
- * Return: Always 0.
- */
+#define MAX_LINE_LENGTH 80
+#define MAX_ARGS 10
+
 int main(void)
 {
-	pid_t my_process_id;
+	char line[MAX_LINE_LENGTH];
+	char *args[MAX_ARGS];
+	int status;
 
-	my_process_id = getpid();
-	printf("%u\n", my_pid);
+	do {
+		printf("shell> ");
+		fgets(line, MAX_LINE_LENGTH, stdin);
+
+		int i = 0;
+		args[i] = strtok(line, " \n");
+		while (args[i] != NULL)
+		{
+			i++;
+			args[i] = strtok(NULL, " \n");
+		}
+		args[i] = NULL;
+
+		if (strcmp(args[0], "cd") == 0)
+		{
+			chdir(args[1]);
+			continue;
+		}
+		else if (strcmp(args[0], "exit") == 0)
+		{
+			exit(0);
+		}
+
+		pid_t pid = fork();
+		if (pid < 0)
+		{
+			perror("fork failed");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			execve(args[0], args, NULL);
+			perror("execve failed");
+			exit(1);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+	} while (1);
+
 	return (0);
 }
